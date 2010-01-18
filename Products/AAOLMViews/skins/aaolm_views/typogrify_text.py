142087b0dda8ad9db64dce64209555d75e586476
Shane Graber <sgraber@gmail.com>
2010-01-18 18:30:40 -0500

    script (python) for using typogrify/smartypants from page templates

---
+++ Products/AAOLMViews/skins/aaolm_views/typogrify_text.py
@@ -0,0 +1,7 @@
+﻿## Script (Python) "typogrify_text"
+##parameters=text
+##title=Returns text that has been run through smartypants and typogrify markup enhancers
+
+from Products.AAOLMViews.typogrify import typogrify
+
+return typogrify(text)
