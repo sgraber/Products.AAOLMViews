e6eb55006fc5cb73278df575fa866e4508ebf314
Shane Graber <sgraber@gmail.com>
2010-01-19 08:55:14 -0500

    removed byte-order-marker from typogrify.py and adjusted call in document_view.pt and it now works

--- Products/AAOLMViews/skins/aaolm_views/typogrify_text.py
+++ Products/AAOLMViews/skins/aaolm_views/typogrify_text.py
@@ -1,12 +1,12 @@
-﻿## Script (Python) "typogrify_text"
+## Script (Python) "typogrify_text"
 ##bind container=container
 ##bind context=context
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=ttext=None
+##parameters=text=None
 ##title=Returns text that has been run through smartypants and typogrify markup enhancers
 
 from Products.AAOLMViews.typogrify import typogrify
 
-return typogrify(ttext)
+return typogrify(text)
