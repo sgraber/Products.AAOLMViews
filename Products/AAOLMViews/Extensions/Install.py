3ceb9d6fd204866fd969a051afef81531bd9c288
Shane Graber <sgraber@gmail.com>
2010-02-02 21:47:21 -0500

    added reinstall=false so reinstalling doesn't remove all customizations

--- Products/AAOLMViews/Extensions/Install.py
+++ Products/AAOLMViews/Extensions/Install.py
@@ -78,94 +78,97 @@ def uninstall(portal, reinstall=False):
     out = StringIO()
     out.write('Uninstalling AAOLMViews\n')
 
-    # remove skins
-    skins_tool = getToolByName(portal, "portal_skins")
-    skin_names = ("aaolm_views")
-    skins = skins_tool.getSkinSelections()
-    for skin in skins:
-        path = skins_tool.getSkinPath(skin)
-        path = map(string.strip, string.split(path,','))
-        path = [s for s in path if s not in skin_names]
-        path = string.join(path, ', ')
-        skins_tool.addSkinSelection(skin, path)
-        print >> out, "Removed '%s' skin from skins_tool." % skin
-
-    # remove our custom views from Topics
-    portal_types = getToolByName(portal, 'portal_types')
-    views = ['blog_view',               # Blog Listing View
-             'toc_view',                # Issue Table of Contents
-             'timelinemap_view',        # Timeline + Map for Events
-             ]
-    old_view = 'folder_summary_view'
-    topic = portal_types.Topic
-    for view in views:    
-        if view in topic.view_methods:
-            # remove installed view from Topic view methods
-            view_methods = [v for v in topic.view_methods if v != view]
-            topic._updateProperty('view_methods', view_methods)
-            print >> out, "Removed '%s' from view_methods for Topics." % view
+    if not reinstall:
+        # remove skins
+        skins_tool = getToolByName(portal, "portal_skins")
+        skin_names = ("aaolm_views")
+        skins = skins_tool.getSkinSelections()
+        for skin in skins:
+            path = skins_tool.getSkinPath(skin)
+            path = map(string.strip, string.split(path,','))
+            path = [s for s in path if s not in skin_names]
+            path = string.join(path, ', ')
+            skins_tool.addSkinSelection(skin, path)
+            print >> out, "Removed '%s' skin from skins_tool." % skin
+    
+        # remove our custom views from Topics
+        portal_types = getToolByName(portal, 'portal_types')
+        views = ['blog_view',               # Blog Listing View
+                 'toc_view',                # Issue Table of Contents
+                 'timelinemap_view',        # Timeline + Map for Events
+                 ]
+        old_view = 'folder_summary_view'
+        topic = portal_types.Topic
+        for view in views:    
+            if view in topic.view_methods:
+                # remove installed view from Topic view methods
+                view_methods = [v for v in topic.view_methods if v != view]
+                topic._updateProperty('view_methods', view_methods)
+                print >> out, "Removed '%s' from view_methods for Topics." % view
+                # reset views to defaults if they used a view from this product
+                brains = portal.portal_catalog.searchResults(portal_type="Topic")
+                for brain in brains:
+                    obj = brain.getObject()
+                    if view in obj.layout:
+                        obj.layout = old_view
+    
+        # remove our custom view from Links
+        view = 'video_view'
+        old_view = 'link_view'
+        portal_types = getToolByName(portal, 'portal_types')
+        link = portal_types.Link
+        
+        if view in link.view_methods:
+            # remove installed view from Link view methods
+            view_methods = [v for v in link.view_methods if v != view]
+            link._updateProperty('view_methods', view_methods) 
+            print >> out, "Removed '%s' view from Link content type." % view
             # reset views to defaults if they used a view from this product
-            brains = portal.portal_catalog.searchResults(portal_type="Topic")
+            brains = portal.portal_catalog.searchResults(portal_type="Link")
             for brain in brains:
                 obj = brain.getObject()
                 if view in obj.layout:
                     obj.layout = old_view
-
-    # remove our custom view from Links
-    view = 'video_view'
-    old_view = 'link_view'
-    portal_types = getToolByName(portal, 'portal_types')
-    link = portal_types.Link
+            print >> out, "Removed '%s' from view_methods for Links and reset them to their default view templates." % view
     
-    if view in link.view_methods:
-        # remove installed view from Link view methods
-        view_methods = [v for v in link.view_methods if v != view]
-        link._updateProperty('view_methods', view_methods) 
-        print >> out, "Removed '%s' view from Link content type." % view
-        # reset views to defaults if they used a view from this product
-        brains = portal.portal_catalog.searchResults(portal_type="Link")
-        for brain in brains:
-            obj = brain.getObject()
-            if view in obj.layout:
-                obj.layout = old_view
-        print >> out, "Removed '%s' from view_methods for Links and reset them to their default view templates." % view
-
-    # remove our custom view from Folders
-    view = 's3slider_view'
-    old_view = 'atct_album_view'
-    portal_types = getToolByName(portal, 'portal_types')
-    folder = portal_types.Folder
+        # remove our custom view from Folders
+        view = 's3slider_view'
+        old_view = 'atct_album_view'
+        portal_types = getToolByName(portal, 'portal_types')
+        folder = portal_types.Folder
+        
+        if view in folder.view_methods:
+            # remove installed view from Folder view methods
+            view_methods = [v for v in folder.view_methods if v != view]
+            folder._updateProperty('view_methods', view_methods) 
+            print >> out, "Removed '%s' view from Folder content type." % view
+            # reset views to defaults if they used a view from this product
+            brains = portal.portal_catalog.searchResults(portal_type="Folder")
+            for brain in brains:
+                obj = brain.getObject()
+                #if view in obj.layout:
+                #    obj.layout = old_view
+            print >> out, "Removed '%s' from view_methods for Folders and reset them to their default view templates." % view
     
-    if view in folder.view_methods:
-        # remove installed view from Folder view methods
-        view_methods = [v for v in folder.view_methods if v != view]
-        folder._updateProperty('view_methods', view_methods) 
-        print >> out, "Removed '%s' view from Folder content type." % view
-        # reset views to defaults if they used a view from this product
-        brains = portal.portal_catalog.searchResults(portal_type="Folder")
-        for brain in brains:
-            obj = brain.getObject()
-            #if view in obj.layout:
-            #    obj.layout = old_view
-        print >> out, "Removed '%s' from view_methods for Folders and reset them to their default view templates." % view
-
-    # remove our custom view from News Items
-    view = 'blog_view'
-    old_view = 'newsitem_view'
-    portal_types = getToolByName(portal, 'portal_types')
-    news_item = getattr(portal_types, 'News Item')
+        # remove our custom view from News Items
+        view = 'blog_view'
+        old_view = 'newsitem_view'
+        portal_types = getToolByName(portal, 'portal_types')
+        news_item = getattr(portal_types, 'News Item')
+        
+        if view in news_item.view_methods:
+            # remove installed view from News Item view methods
+            view_methods = [v for v in news_item.view_methods if v != view]
+            news_item._updateProperty('view_methods', view_methods)
+            # news.allow_discussion = False
+            news_item.default_view = old_view
+            news_item.immediate_view = old_view
+            # reset views to defaults if they used a view from this product
+            brains = portal.portal_catalog.searchResults(portal_type="News Item")
+            for brain in brains:
+                obj = brain.getObject()
+                if view in obj.layout:
+                    obj.layout = old_view        
+            print >> out, "Removed '%s' from view_methods for News Items and reset them to their default view templates." % view
     
-    if view in news_item.view_methods:
-        # remove installed view from News Item view methods
-        view_methods = [v for v in news_item.view_methods if v != view]
-        news_item._updateProperty('view_methods', view_methods)
-        # news.allow_discussion = False
-        news_item.default_view = old_view
-        news_item.immediate_view = old_view
-        # reset views to defaults if they used a view from this product
-        brains = portal.portal_catalog.searchResults(portal_type="News Item")
-        for brain in brains:
-            obj = brain.getObject()
-            if view in obj.layout:
-                obj.layout = old_view        
-        print >> out, "Removed '%s' from view_methods for News Items and reset them to their default view templates." % view
+    return out.getvalue()
