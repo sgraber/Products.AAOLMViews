6691ff5168a6db5a58c86a39770be39f65bef4e7
Shane Graber <sgraber@gmail.com>
2010-09-26 10:14:46 -0400

    added install.txt, license.txt, history.txt to long_description

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(name='Products.AAOLMViews',
       version=version,
       description="Simple product for installing a couple custom views for Advanced Aquarist.",
       long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "INSTALL.txt")).read()  + "\n" +
+                       open(os.path.join("docs", "LICENSE.txt")).read()  + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
